c6a7657aefa4068dfbf063521f275b8fa29adc85
Sharif Ahmed <sharifahmedewu15@gmail.com>
2021-04-16 11:30:03 +0600

    add heroku

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!e22pb6nj-*5(as@5!vii$vlc-t#2#-u^^gm+5lp5(eng3v8x8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shoppingly699.herokuapp.com','127.0.0.1']
 
 
 # Application definition
